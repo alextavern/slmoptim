corrected normalization of tm

optimization/cost_functions.py
@@ -40,4 +40,4 @@ def snr(frame, mask_radius=8, mask_offset=(0, 0), intesity_only=False):
         noise = np.sum((frame) * (1. - mask)) / np.sum(1. - mask)
         cost = signal / noise
 
-    return cost    
+    return cost, mask    

optimization/iteration_algorithms.py
@@ -286,10 +286,10 @@ class HadamardPartition(IterationAlgos):
     
     def run():
         pass
-    
-    
 
-                
+""" This class is implementing an idea found here https://www.wavefrontshaping.net/post/id/23
+    that uses Zenike Polynomials to optimize optical aberrations on a focused laser bea,
+"""
 class ZernikePolynomials():
     
     def __init__(self, 
@@ -355,6 +355,7 @@ class ZernikePolynomials():
     
     def _get_disk_mask(self, center = None):
         '''
+        Taken from S. Popoff blog
         Generate a binary mask with value 1 inside a disk, 0 elsewhere
         :param shape: list of integer, shape of the returned array
         :radius: integer, radius of the disk
@@ -371,6 +372,7 @@ class ZernikePolynomials():
         
     def _complex_mask_from_zernike_coeff(self, vec):
         '''
+        Taken from S. Popoff blog
         Generate a complex phase mask from a vector containting the coefficient of the first Zernike polynoms.
         :param DMD_resolution: list of integers, contains the resolution of the DMD, e.g. [1920,1200]
         :param: integer, radius of the illumination disk on the DMD
@@ -379,13 +381,11 @@ class ZernikePolynomials():
         '''
         # Generate a complex phase mask from the coefficients
         zern_mask = np.exp(1j * phaseFromZernikes(vec, 2 * self.radius))
-        
-        gauss_mask = np.exp(1j * self._phaseFromGauss(vec, 2 * self.radius))
-        
+                
         # We want the amplitude to be 0 outside the disk, we fist generate a binary disk mask
         amp_mask = self._get_disk_mask()
         
-        # put the Zernik mask at the right position and multiply by the disk mask
+        # put the Zernike mask at the right position and multiply by the disk mask
         mask = np.zeros(shape = self.shape, dtype='complex')
         mask[self.center[0] - self.radius:self.center[0] + self.radius,
              self.center[1] - self.radius:self.center[1] + self.radius] = zern_mask * amp_mask
@@ -393,9 +393,11 @@ class ZernikePolynomials():
         return mask
     
     def _phase2SLM(self, mask):
+        """ Converts a phase mask to the SLM readable  and phase-calibrated format
+        """
         arg = np.angle(mask, deg=False)
         # scale phase between 0 and 2pi
-        arg2pi = (arg + 2 * np.pi) % (2 * np.pi)
+        # arg2pi = (arg + 2 * np.pi) % (2 * np.pi)
         arg2pi = arg + np.pi
         # normalize to SLM 2pi calibration value
         arg2SLM = arg2pi * self.calib_px / (2 * np.pi) 
@@ -410,14 +412,16 @@ class ZernikePolynomials():
         masks = {}
         cost = []
         
+        # initialize the coefficients to optimize
         coeffs = np.zeros(self.num_of_zernike_coeffs)
-        phi_k = np.arange(coeff_range[0], coeff_range[1], coeff_range[2])
+        # 
+        coeff_idx = np.arange(coeff_range[0], coeff_range[1], coeff_range[2])
         
         iterator = trange(self.num_of_zernike_coeffs)
         for idx in iterator:
             cost_temp = []
-            for phi in phi_k:
-                coeffs[idx] = phi
+            for coeff in coeff_idx:
+                coeffs[idx] = coeff
                 zmask = self._complex_mask_from_zernike_coeff(coeffs)
                 zmask = self._phase2SLM(zmask)
                 self.upload_pattern(zmask, 0.1)
@@ -431,7 +435,7 @@ class ZernikePolynomials():
 
             # update pattern with max corr
             cost.append(np.max(cost_temp))            
-            coeffs[idx] = phi_k[np.argmax(cost_temp)]
+            coeffs[idx] = coeff_idx[np.argmax(cost_temp)]
             
             # just reload the optimal mask for this iteration and save 
             # the corresponding frame

optimization/phase_conjugation.py
@@ -21,13 +21,29 @@ class Target:
         
         return target_frame
 
-    def gauss(self):
-        pass
+    def gauss(self, 
+              order=0,
+              num=1, 
+              w0=4e-4, 
+              wavelength=632e-9, 
+              size=15e-3, 
+              slm_calibration_px=112):
+                 
+        N = int(self.shape[0])
+        gauss = pt.GaussPatternGenerator(N=N, num=num, LG=True,
+                                        w0=w0, 
+                                        wavelength=wavelength, 
+                                        size=size, 
+                                        slm_calibration_px=slm_calibration_px) 
+        amp, phase = gauss[order]
+        complex_field = amp * np.exp(1j * phase)
+        
+        return complex_field
     
     
 class InverseLight:
     
-    def __init__(self, target, tm, calib_px=112, slm_macropixel=4):
+    def __init__(self, target, tm, calib_px=56, slm_macropixel=4):
 
         self.target = target
         self.shape = target.shape
@@ -46,20 +62,20 @@ class InverseLight:
         """
         self.tm_T_star = self.tm.transpose().conjugate()
         return self.tm_T_star
+    
+    def _inverse(self):
+        self.tm_inv = np.linalg.inv(self.tm)
+        return self.tm_inv
+
         
     def calc_inv_operator(self):
         inv_operator = self.tm@self.tm_T_star
         return inv_operator
 
-
     
-    def inverse_prop(self):
-        """ Calculates the inverse light propagation and produces a phase mask
-
-        Returns
-        -------
-        _type_
-            _description_
+    def inverse_prop(self, conj=True):
+        """ Calculates the inverse light propagation and produces a phase mask.
+            User must define inversion method: phase conjugation or matrix inversion.
         """
         
         # first flatten frame
@@ -70,9 +86,14 @@ class InverseLight:
         target_frame_flattened = np.array(target_frame_flattened)
 
         # apply inversion
-        # tm_T_star = self._conj_trans()
-        tm_T_star = self.tm.transpose().conjugate()
-        inverse = np.dot(tm_T_star, target_frame_flattened.T)
+        if conj:
+            # tm_T_star = self._conj_trans()
+            tm_inv = self._conj_trans()
+        else:
+            tm_inv = self._inverse()
+            
+        # inverse = np.dot(tm_T_star, target_frame_flattened.T)
+        inverse = tm_inv@target_frame_flattened.T
 
         # get phase (in -pi to pi)
         arg = np.angle(inverse, deg=False)

optimization/transmission_matrix.py
@@ -19,9 +19,9 @@ class measTM:
 
     def __init__(self, 
                  slm, camera, 
-                 pattern_loader,
-                 num_in, 
+                 pattern_loader, 
                  slm_macropixel_size,
+                 slm_resolution=(800, 600),
                  calib_px=112,
                  remote=True, 
                  corr_path=None,
@@ -30,14 +30,15 @@ class measTM:
 
         Parameters
         ----------
-        roi
-        bins
-        exposure_time
-        gain
-        timeout
-        order
-        mag
-        monitor
+        slm
+        camera
+        pattern loader
+        slm macropixel
+        slm resolution
+        slm calibration pixel
+        remote operation
+        correction pattern path
+        save data path
         """
         
         # camera
@@ -45,9 +46,10 @@ class measTM:
         
         # slm settings
         self.slm = slm
-        self.num_in = num_in
+        self.num_in = len(pattern_loader)
         self.slm_macropixel_size = slm_macropixel_size
         self.calib_px = calib_px
+        self.resX, self.resY = slm_resolution
         self.remote = remote # is the slm connected to a raspberry Pi ?
         
         # load basis patterns
@@ -60,12 +62,8 @@ class measTM:
         self.save_path = save_path
         
     def get(self):
-        """_summary_
-
-        Returns
-        -------
-        _type_
-            _description_
+        """ Opens two parallel threads one to upload patterns to the SLM and one to download frames
+            from the camera
         """
         
         # Create flag events
@@ -96,13 +94,6 @@ class measTM:
         # Wait for the threads to finish
         download_thread.join()
         upload_thread.join()
-
-        # The main thread will wait for both threads to finish before continuing
-        
-        # Finally, kill all
-        # self.slm.close()
-        # self.init_camera.destroy()
-        # print("Program execution completed - camera and slm killed! ")
         
         # get and return data 
         self.patterns = upload_thread.patterns
@@ -111,57 +102,56 @@ class measTM:
         return self.patterns, self.frames
     
     def get2(self, slm_delay=0.1):
-        """_summary_
+        """ A simpler implemetantion of the TM acquisition where only a time delay is introduced between 
+            uploading and downloading in order to make sure that the pattern is uploaded before acquiring
+            a frame
 
         Parameters
         ----------
         slm_delay : float, optional
-            _description_, by default 0.1
+            time delay between uploading and downloading, by default 0.1
 
         Returns
         -------
-        _type_
-            _description_
+        frames : list
+            a list of interferogram frames            
         """
-        # basis = pt.Pattern._get_hadamard_basis(self.num_in)
+        
         pi = int(self.calib_px / 2)
         four_phases = [0, pi / 2, pi, 3 * pi / 2]
         
-        # four_intensities = {}
-        # self.patterns = []
         self.frames = []
         
-        # resX, resY = self.slm.getSize()
-        # resX, resY = (800, 600)
-        # slm_patterns = pt.Pattern(resX, resY)
+        resX, resY = (800, 600)
+        slm_patterns = pt.Pattern(resX, resY)
 
 
         # loop through each 2d vector of the loaded basis
-        # for vector in tqdm(basis, desc='Uploading Hadamard patterns', leave=True):
         for vector in tqdm(self.pattern_loader, desc="Uploading pattern vectors", leave=True):
             # and for each vector load the four reference phases
             for phase in four_phases:
-                # _, pattern = slm_patterns.hadamard_pattern_bis(vector, n=self.slm_macropixel_size, gray=phase)
+                pattern = slm_patterns.enlarge_to_SLM(vector, self.slm_macropixel_size, phase)
                 if self.remote:
-                    self.slm.sendArray(vector)
+                    self.slm.sendArray(pattern)
                 else:
-                    self.slm.updateArray(vector) # load each vector to slm
-                time.sleep(slm_delay)
+                    self.slm.updateArray(pattern) # load each vector to slm
+                time.sleep(slm_delay) # wait to make sure the vector is loaded
+                
                 # get frame for each phase
                 frame = self.camera.get_pending_frame_or_null()
                 image_buffer_copy = np.copy(frame.image_buffer)
 
-                # four_intensities[idx, phase] = image_buffer_copy
-                # self.patterns.append(vector)
                 self.frames.append(image_buffer_copy)
                 
 
         print("ΤΜ acquisition completed ! ")
             
-        # return self.patterns, self.frames
         return self.frames
         
     def save(self):
+        """ creates a filename ans saves raw data
+            to a pickle format
+        """
 
         timestr = time.strftime("%Y%m%d")
         new_path = os.path.join(self.save_path, timestr)
@@ -192,7 +182,6 @@ class calcTM:
 
     def __init__(self, data):
         self.data = data
-        
     
     @staticmethod
     def four_phases_method(intensities):
@@ -224,7 +213,7 @@ class calcTM:
         iterator = [iterator[n:n+4] for n in range(0, len(iterator), 4)]
 
         # initialize a 2d complex matrix: the observed transmission matrix
-        tm_obs = np.full(shape=(cam_px_len, slm_px_len), fill_value=0).astype('complex128')
+        self.tm_obs = np.full(shape=(cam_px_len, slm_px_len), fill_value=0).astype('complex128')
         
         # loop through every pixel of a camera frame (2d array)
         cam_px_idx = 0
@@ -239,47 +228,42 @@ class calcTM:
                 # calculate the complex field value
                 four_phases = self.four_phases_method(four_intensities_temp)
                 # save it into the transmission matrix
-                tm_obs[cam_px_idx, slm_px_idx] = four_phases
+                self.tm_obs[cam_px_idx, slm_px_idx] = four_phases
                 # increment pixel indices
                 slm_px_idx += 1
             cam_px_idx += 1
         
-        return tm_obs
+        return self.tm_obs
+    
+    def _normalization_factors(self):
+        """ Calculates the standard deviation of each row of the observed 
+            hadamard-based transmission matrix and returns a diagonal matrix
+            whose elements give the std
 
-
-    def _normalization_factor(self):
-
-        # get dimensions and length that will be useful for the for loops
-        total_num, frame_shape, slm_px_len, cam_px_len = self._calc_dim()
-        # organize iterator over all frames into group of 4. Each group corresponds to 
-        # the one 4-phase measurement, i.e. one slm vector with 4 grayscale levels
-        iterator = np.arange(0, total_num)
-        iterator = [iterator[n:n+4] for n in range(0, len(iterator), 4)]
-
-        # initialize a 2d matrix: 
-        norm_ij = np.full(shape=(cam_px_len, slm_px_len), fill_value=0).astype('float64')
+        Returns
+        -------
+            norm_factors: diagonal 2d array - each element is the std of the TM
+        """
+        tm = abs(self.tm_obs)
         
-        # loop through every pixel of a camera frame (2d array)
-        cam_px_idx = 0
-        for iy, ix in np.ndindex(frame_shape):
-            slm_px_idx = 0
-            cam_px = []
-            # loop through every 4-phase measurement - equivalently every slm pixel
-            for subiterator in iterator:
-                cam_amp_temp = []
-                # create a temp list with four intensities
-                for subsub in subiterator:
-                    cam_amp_temp.append(self.data[subsub][iy, ix])
-                # calculate the complex field value
-                cam_px.append(cam_amp_temp[3])
-                # increment pixel indices
-                slm_px_idx += 1
-
-            std = np.array(cam_px).std()
-            norm_ij[cam_px_idx, :] = std
-            cam_px_idx += 1
-
-        return norm_ij
+        # Get means for each row
+        means = [row.mean() for row in tm]
+        # Calculate squared errors
+        squared_errors = [(row - mean) ** 2 for row, mean in zip(tm, means)]
+        # Calculate the inverse square root of mean for each row of squared errors 
+        # this is the inverse standard deviation of each row
+        norm_factors = np.diag([(1 / row.mean()) ** 0.5 for row in squared_errors])
+        
+        return norm_factors
+        
+    def _normalize(self):
+        """ Normalizes the observed TM by computing its dot product with the normalization
+            factors diagonal matrix
+        """
+        norm_factors = self._normalization_factors()
+        tm_fil = norm_factors@self.tm_obs
+        # tm_fil = np.dot(norm_factors, self.tm_obs)
+        return tm_fil
     
     def _had2canonical(self, matrix):
         """ Perform a basis change: from the hadamard to the canonical one
@@ -291,27 +275,24 @@ class calcTM:
         tm_can = np.dot(matrix, h)
         return tm_can
 
-    def calc_plot_tm(self):
+    def calc_plot_tm(self, figsize=(10, 5)):
         
         tm_obs = self._calc_obs()
-        norm = self._normalization_factor()
-        tm_fil = tm_obs / norm
+        tm_fil = self._normalize()
         tm = self._had2canonical(tm_fil)
         
-        fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True, figsize=(7, 7))
-        axs[0, 0].imshow(abs(tm_obs), aspect='auto')
-        axs[1, 0].imshow(norm, aspect='auto')
-        axs[0, 1].imshow(abs(tm_fil), aspect='auto')
-        axs[1, 1].imshow(abs(tm), aspect='auto')
+        fig, axs = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True, figsize=figsize)
+        axs[0].imshow(abs(tm_obs), aspect='auto')
+        axs[1].imshow(abs(tm_fil), aspect='auto')
+        axs[2].imshow(abs(tm), aspect='auto')
 
-        axs[0, 0].set_title("Hadamard TM")
-        axs[1, 0].set_title("Normalization")
-        axs[0, 1].set_title("Filtered TM")
-        axs[1, 1].set_title("Canonical TM")
+        axs[0].set_title("Hadamard TM")
+        axs[1].set_title("Filtered TM")
+        axs[2].set_title("Canonical TM")
 
         fig.text(0.5, -0.01, 'slm pixels #', ha='center')
         fig.text(-0.01, 0.5, 'camera pixels #', va='center', rotation='vertical')
         fig.tight_layout()
         
-        return tm_obs, norm, tm_fil, tm   
+        return tm_obs, tm_fil, tm   
              

patternSLM/patterns.py
@@ -82,6 +82,28 @@ class Pattern:
         pattern[:cols, int(rows / a):] = int(gray2)
 
         return pattern.astype('uint8')
+    
+    def grad_mirror(self, axis=0):
+        """
+        creates a gradient mirror
+
+        Parameters
+        ----------
+        axis: int
+
+        Returns
+        -------
+        pattern: 2d array
+        """
+        gray = self.grayphase
+        x = np.linspace(0, gray, 800)
+        y = np.linspace(0, gray, 600)
+        X, Y = np.meshgrid(x, y)
+        
+        if axis == 0: pattern = X
+        else: pattern = Y
+
+        return pattern.astype('uint8')
 
     def grating(self, sq_amp=255, sq_period=100):
         """
@@ -223,13 +245,14 @@ class Pattern:
 
         return hadamard_vector, pattern.astype('uint8')
 
-    def pattern2SLM(self, vector, n=1, gray=0):
+    def enlarge_to_SLM(self, vector, n=1, gray=0):
         """
-        creates a hadamard vector and puts it in the middle of the slm screen
+        puts an enlarged vector in the middle of the slm screen
+        
         Parameters
         ----------
-        vector: one hadamard vector
-        n: hadamard vector magnification factor
+        vector: input vector
+        n: vector magnification factor
         gray: grayscale level of the unaffected slm screen (int)
 
         Returns
@@ -256,7 +279,7 @@ class Pattern:
         # and then add the vector in the center of the initialized pattern
         pattern[offset_y:offset_y + subpattern_dim[0], offset_x:offset_x + subpattern_dim[1]] = vector
 
-        return vector, pattern.astype('uint8')
+        return pattern.astype('uint8')
     
     def random(self, dim):
         """_summary_
@@ -418,6 +441,23 @@ class OnePixelPatternGenerator:
     def __len__(self):
         return len(self.patterns)
     
+    def _get_disk_mask(self, center = None):
+        '''
+        Taken from S. Popoff blog
+        Generate a binary mask with value 1 inside a disk, 0 elsewhere
+        :param shape: list of integer, shape of the returned array
+        :radius: integer, radius of the disk
+        :center: list of integers, position of the center
+        :return: numpy array, the resulting binary mask
+        '''
+        shape = [2 * self.radius] * 2
+        if not center:
+            center = (shape[0] // 2, shape[1] // 2)
+        X, Y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]))
+        mask = (Y - center[0]) ** 2 + (X - center[1]) ** 2 < self.radius ** 2
+        
+        return mask.astype('bool')
+    
     def _get_random_pixels(self):
         """ creates all indices of a 2d matrix at a random order
             in order to later sample randomly the pixels of a given mask
@@ -447,6 +487,7 @@ class OnePixelPatternGenerator:
         masks = []
         for i, j in self.random_idx:
             mask = np.zeros(self.N ** 2, dtype=bool )
+            mask = self._get_disk_mask()
             new_dim = int(self.N)
             mask = mask.reshape(new_dim, new_dim)
             